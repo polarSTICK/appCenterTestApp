568d68a329762885680307fa97fe2c7a19aaa295
Mikael Mäkelä <makelamikael1@gmail.com>
2017-12-28 13:52:28 +0200

    added new analytics tracking

--- appCenterTestApp/appCenterTestApp/App.xaml.cs
+++ appCenterTestApp/appCenterTestApp/App.xaml.cs
@@ -19,17 +19,21 @@ namespace appCenterTestApp
 		{
 		    AppCenter.Start("android=27993508-c148-409b-95cf-d2dd4d965f77;" + "uwp=0cf6d621-48d9-4280-bcc8-e14b19cbe1ce;" +
                             "ios=f1d4dc5b-3b4c-4c69-ad25-0c8dc46d2baa;", typeof(Analytics), typeof(Crashes));
+
+            Analytics.TrackEvent("Application start");
             // Handle when your app starts
         }
 
 		protected override void OnSleep ()
 		{
-			// Handle when your app sleeps
-		}
+            Analytics.TrackEvent("Application hidden");
+            // Handle when your app sleeps
+        }
 
 		protected override void OnResume ()
 		{
-			// Handle when your app resumes
-		}
+            Analytics.TrackEvent("Application resumed");
+            // Handle when your app resumes
+        }
 	}
 }
